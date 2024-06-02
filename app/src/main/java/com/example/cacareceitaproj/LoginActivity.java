50e24b2c58e70cabda5185b17cdc8a9360117d9a
IsabelaSilvaz <isabelasilvap778@gmail.com>
2024-06-01 23:46:32 -0300

    Criação do projeto

---
+++ app/src/main/java/com/example/cacareceitaproj/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.cacareceitaproj;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.cacareceitaproj.activity.CadastroActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void cadastrar(View v) {
+        Intent i = new Intent(this, CadastroActivity.class);
+        startActivity(i);
+    }
+}
