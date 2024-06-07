e7f5db30714200d7da3292dd835d0511f8b65202
luiisaferreira <luiisaferreira@hotmail.com>
2024-06-07 00:21:35 -0300

    adicionando tela de PagamentoCartao.

--- app/src/main/java/com/example/cacareceitaproj/activity/PagamentoCartao.java
+++ app/src/main/java/com/example/cacareceitaproj/activity/PagamentoCartao.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.cacareceitaproj.R;
 
-public class RemoverReceita extends AppCompatActivity {
+public class PagamentoCartao extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_remover_receita);
+        setContentView(R.layout.activity_pagamento_cartao);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
